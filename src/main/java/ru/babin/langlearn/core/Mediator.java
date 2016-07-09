c352f4e7c43586acf63278dbe0590f5965f0bf1f
Alexander Babin <alexander.babin@gmail.com>
2016-07-09 23:21:50 +0300

    next comit

--- src/main/java/ru/babin/langlearn/core/Mediator.java
+++ src/main/java/ru/babin/langlearn/core/Mediator.java
@@ -1,55 +1,51 @@
 package ru.babin.langlearn.core;
 
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
-
-import ru.babin.langlearn.ui.FrmMain;
-import ru.babin.langlearn.ui.PnlBottom;
-import ru.babin.langlearn.ui.PnlMiddle;
-import ru.babin.langlearn.ui.PnlTop;
 
 public class Mediator {
     
-    FrmMain    mainForm;
-    PnlTop     panelTop;
-    PnlMiddle  panelMiddle;
-    PnlBottom  panelBottom;
-           
+    // ------------------------------------------------------------------------------------------------------------------
+    private javax.swing.JList<String> lstUnknownWords;
+    public void setLstUnknownWords(javax.swing.JList<String> lstUnknownWords){this.lstUnknownWords = lstUnknownWords;}
+    
+    private javax.swing.JLabel lblUnknownWordsCount;
+    public void setLblUnknownWordsCount(javax.swing.JLabel lblUnknownWordsCount){this.lblUnknownWordsCount = lblUnknownWordsCount;}
+    
+    private javax.swing.JLabel lblVocabularyInfo;
+    public void setLblVocabularyInfo(javax.swing.JLabel lblVocabularyInfo){this.lblVocabularyInfo = lblVocabularyInfo;}
+    // ------------------------------------------------------------------------------------------------------------------
+    
+    Vocabulary vocabulary = new Vocabulary();
+    
+    public void init(){
+        try {
+            vocabulary.load();
+            lblVocabularyInfo.setText(vocabulary.getVocabulary().size() + "");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+            
+    public void eventNewLoadedWords(List <String> words){
+        int foundWordsCount = words.size();
+                
+        List <String> onlyUnknownWords = new LinkedList<>();
+        for(String word : words){
+            if(!vocabulary.getVocabulary().contains(word.toLowerCase())){
+                onlyUnknownWords.add(word);
+            }
+        }
+        lstUnknownWords.setListData(onlyUnknownWords.toArray(new String[onlyUnknownWords.size()]));
+        setUnknownWordsCounter(onlyUnknownWords.size(), foundWordsCount);
+    }
+    
+    public void setUnknownWordsCounter(int unknownWords , int totalWords){
+        lblUnknownWordsCount.setText(String.valueOf(unknownWords) + " from " + totalWords);
+    }
+       
         
-    public void registerMainForm(FrmMain mainForm){
-        this.mainForm = mainForm;
-    }
-    
-    public void registerPanelTop(PnlTop panelTop){
-        this.panelTop = panelTop;
-    }
-    
-    public void registerPanelBottom(PnlBottom panelBottom){
-        this.panelBottom = panelBottom;
-    }
-    
-    public void registerPanelMiddle(PnlMiddle panelMiddle){
-        this.panelMiddle = panelMiddle;
-    }
-    
-    public void setLoadedWords(List <String> words){
-        panelMiddle.setFoundWords(words);
-    }
-    
-    public void setVocabularySize(int vocSize){
-        panelBottom.setVocabularySize(vocSize);
-    }
-    
-    public void setUnknownWordsCounter(int size){
-        panelBottom.setUnknownWordsCount(size);
-    }
-    
-    public Vocabulary getVocabulary(){
-        return mainForm.getVocabulary();
-    }
-
-    public void setFoundWordsCount(int foundWordsCount) {
-        panelBottom.setFoundWordsCount(foundWordsCount);
-        
-    }
-    
 }
